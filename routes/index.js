7a001df6d3385b5df8c4b0b674ffa03e01b80a1a
tomokazukozuma <tomokazu.kozuma@gmail.com>
2015-03-10 11:11:00 +0900

    delete

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,6 @@ var async = require('async');
 var _ = require('underscore');
 var validator = require('validator');
 
-
-// helper
-var helper = require('helpers/applicationHelper');
-
 exports.init = function(app){
 
     // TOPページを取得
